f3e62e832cdb3f109b0fdedf6720e5bdf17994f9
chris-gong <chris_gong@yahoo.com>
2018-10-02 23:50:08 -0400

    Uploaded initial project

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour{
+    private bool moved = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("x")&&!moved)
+        {
+            moved = true;
+            Vector3 newpos = new Vector3(2.32f, 1.0f, -2.16f);
+            gameObject.transform.position = newpos;
+            gameObject.transform.Rotate(Vector3.up * 90);
+        }
+	}
+}
